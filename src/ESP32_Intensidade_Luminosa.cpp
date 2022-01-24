ab75d82b5fd5717071d9981d702b1d04eabd55f7
FishLabPamp <fishlabpampulha@gmail.com>
2022-01-23 23:15:11 -0300

    Inclusão Brief

--- src/ESP32_Intensidade_Luminosa.cpp
+++ src/ESP32_Intensidade_Luminosa.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
-
+/**
+ * @brief Branch Configuração_Sensor
+ * Responsável apenas pela configuração da sonda, para que possa medir a intensidade luminosa solar próximo ao sensor DALLAS na mangueira do aquecedor solar.
+ * 
+ */
 void setup() {
   // put your setup code here, to run once:
 }
